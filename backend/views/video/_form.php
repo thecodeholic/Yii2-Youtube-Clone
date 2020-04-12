770a82a25e66475c58026bdc0dad7c456e48f2cc
Zura Sekhniashvili <zurasekhniashvili@gmail.com>
2020-04-12 15:26:47 +0400

    Modify video update page

--- backend/views/video/_form.php
+++ backend/views/video/_form.php
@@ -12,23 +12,22 @@ use yii\bootstrap4\ActiveForm;
 
     <?php $form = ActiveForm::begin(); ?>
 
-    <?= $form->field($model, 'video_id')->textInput(['maxlength' => true]) ?>
+    <div class="row">
+        <div class="col-sm-8">
+            <?= $form->field($model, 'title')->textInput(['maxlength' => true]) ?>
 
-    <?= $form->field($model, 'title')->textInput(['maxlength' => true]) ?>
+            <?= $form->field($model, 'description')->textarea(['rows' => 6]) ?>
 
-    <?= $form->field($model, 'description')->textarea(['rows' => 6]) ?>
+            <?= $form->field($model, 'tags')->textInput() ?>
 
-    <?= $form->field($model, 'tags')->textarea(['rows' => 6]) ?>
-
-    <?= $form->field($model, 'status')->textInput() ?>
-
-    <?= $form->field($model, 'video_name')->textInput(['maxlength' => true]) ?>
-
-    <?= $form->field($model, 'created_at')->textInput() ?>
-
-    <?= $form->field($model, 'updated_at')->textInput() ?>
-
-    <?= $form->field($model, 'created_by')->textInput() ?>
+            <?= $form->field($model, 'status')->textInput() ?>
+        </div>
+        <div class="col-sm-4">
+            <div class="embed-responsive embed-responsive-16by9">
+                <video class="embed-responsive-item" src="https://www.youtube.com/embed/zpOULjyy-n8?rel=0"></video>
+            </div>
+        </div>
+    </div>
 
     <div class="form-group">
         <?= Html::submitButton('Save', ['class' => 'btn btn-success']) ?>
