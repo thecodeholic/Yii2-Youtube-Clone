bae6cb55cfdbbf743c013b5d22a627c0cda73397
Zura Sekhniashvili <zurasekhniashvili@gmail.com>
2020-04-20 11:05:48 +0400

    Add check if latest video exists on backend

--- backend/views/site/index.php
+++ backend/views/site/index.php
@@ -10,23 +10,29 @@ $this->title = 'My Yii Application';
 ?>
 <div class="site-index d-flex">
     <div class="card m-2" style="width: 14rem;">
-        <div class="embed-responsive embed-responsive-16by9 mb-3">
-            <video class="embed-responsive-item"
-                   poster="<?php echo $latestVideo->getThumbnailLink() ?>"
-                   src="<?php echo $latestVideo->getVideoLink() ?>"></video>
-        </div>
-        <div class="card-body">
-            <h6 class="card-title"><?php echo $latestVideo->title ?></h6>
-            <p class="card-text">
-                Likes: <?php echo $latestVideo->getLikes()->count() ?><br>
-                Views: <?php echo $latestVideo->getViews()->count() ?>
-            </p>
-            <a href="<?php echo \yii\helpers\Url::to(['/video/update',
-                'id' => $latestVideo->video_id]) ?>"
-               class="btn btn-primary">
-                Edit
-            </a>
-        </div>
+        <?php if ($latestVideo): ?>
+            <div class="embed-responsive embed-responsive-16by9 mb-3">
+                <video class="embed-responsive-item"
+                       poster="<?php echo $latestVideo->getThumbnailLink() ?>"
+                       src="<?php echo $latestVideo->getVideoLink() ?>"></video>
+            </div>
+            <div class="card-body">
+                <h6 class="card-title"><?php echo $latestVideo->title ?></h6>
+                <p class="card-text">
+                    Likes: <?php echo $latestVideo->getLikes()->count() ?><br>
+                    Views: <?php echo $latestVideo->getViews()->count() ?>
+                </p>
+                <a href="<?php echo \yii\helpers\Url::to(['/video/update',
+                    'id' => $latestVideo->video_id]) ?>"
+                   class="btn btn-primary">
+                    Edit
+                </a>
+            </div>
+        <?php else: ?>
+            <div class="card-body">
+                You don't have uploaded videos yet
+            </div>
+        <?php endif; ?>
     </div>
     <div class="card m-2" style="width: 14rem;">
         <div class="card-body">
